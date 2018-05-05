92f2d2aa53963b3f9973890c0bd7d9b6694a7a8d
kuo weiti <weiti.kuo@gmail.com>
2018-05-05 16:33:46 +0800

    test for the diff with Ubnutu

--- iPeenCrawler.py
+++ iPeenCrawler.py
@@ -135,6 +135,10 @@ if business_hour_div != None:
         day_info = item.findAll("td")
         day_of_week = remove_html_tags(day_info[0].renderContents().strip('\n'))
         print "day_of_week = " + day_of_week.strip('\n')
+
+        print "Test: -----------"
+        print str(day_info[1].renderContents().strip('\n'))
+
         day_working_hour = str(day_info[1].renderContents().strip('\n')).split("br/")
         first_time_slot = re.sub(r'( |<|\r|\n|\t|&nbsp;)+', r'', remove_html_tags(day_working_hour[0]).strip('\n'))
         print "first_time_slot = " + first_time_slot
